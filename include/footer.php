a690eb1691873f37a79f32ae62956acf11dc6d2c
flaapu <flaviacrolla@hotmail.es>
2020-09-25 02:28:26 -0300

    footer y portada

--- include/footer.php
+++ include/footer.php
@@ -12,33 +12,111 @@
 
 <body>
 
-<footer class="foot">
+<!-- Footer -->
+<footer class="page-footer font-small special-color-dark pt-4">
+     <!-- Footer Links -->
+     <div class="container-fluid text-center text-md-left">
+  <!-- Footer Elements -->
+  <div class="container">
+  <div class="conteiner-searching">
+    <!--Grid row-->
+    <div class="row">
 
-    <div class="container px-lg-5">
-        <div class="row mx-lg-n5">
-            <div class="col-sm-6 col-md-8">
-                   <div class="texto-copyright">
-                        <p>  
-                        Copyright &copy; 1995-<?php echo date("Y"); ?>.  Todos los derechos reservados. Conoce nuestra Política de privacidad. <br>
-                        FLAAPU S.R.L Argentina. Avenida Siempre Viva 742 CP: 3100 Paraná. Entre Ríos, Argentina.<br>
-                        • <b>Atención al cliente</b>: Vos manda que yo cualquier cosa te contesto •
-                         </p>
-                    </div>
-            
-            </div>
-            <div class="col-6 col-md-4">
-                <div class="texto-contacto">
-                    <p>Contacto:
-                    <br>
-                    <a href="mailto:flaviacrolla@hotmail.es"><img width="30" height="30" src="img/correo-electronico.svg" alt="Correo"/>Correo</a><br>
-                    <a href="https://www.google.com.ar/maps/place/Facultad+de+Ciencia+y+Tecnolog%C3%ADa+(UADER)/@-31.8335955,-60.5221568,17.81z/data=!4m5!3m4!1s0x95b44b973b15bedd:0xa59a2314fe8063f2!8m2!3d-31.8341498!4d-60.5219188"><img width="30" height="30" src="img/localizar.svg" alt="Ubicacion"/>FCyT</a> 
-                    </p>
-                </div>
-            </div>
-        </div>
+
+      <!--Grid column-->
+      <div class="col-md-6 mb-4">
+      
+        <!-- Form -->
+        <form class="form-inline">
+          <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Buscar"
+            aria-label="Buscar">
+          <i class="fas fa-search" aria-hidden="true"></i>
+        </form>
+        <!-- Form -->
+
+      </div>
+      
+
+      <!--Grid column-->
+      <div class="col-md-6 mb-4">
+
+        <form class="input-group">
+          <input type="text" class="form-control form-control-sm" placeholder="Ingresa tu usuario o email"
+            aria-label="Ingresa tu usuario o email" aria-describedby="basic-addon2">
+          <div class="input-group-append">
+            <button class="buttons2" type="button">Ingresar</button>
+          </div>
+        </form>
+        
+      </div>
+      <!--Grid column-->
+
     </div>
+    <!--Grid row-->
+    </div> <!-- div searching-->
+  </div>
+  <!-- Footer Elements -->
+    <hr>
+  <!-- Footer Links -->
+  <div class="conteiner-midbox">
+  <div class="container-fluid">
 
-</footer>
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-3">
+        <br>
+      <h5 class="text-uppercase">POLITICS & PRIVACITY ndea </h5>
+            <p> Todos los derechos reservados. Conoce nuestra <a href="./politica_privacidad">Política de privacidad</a>.<br>
+                FLAAPU S.R.L Argentina. Avenida Siempre Viva 742 <br> CP: 3100 Paraná. Entre Ríos, Argentina.<br>
+                • <b>Atención al cliente</b>: Vos manda que yo cualquier cosa te contesto •
+            </p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none ">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-3">
+
+        <!-- Links -->
+        <br>
+        <h5 class="text-uppercase">Links</h5>
+
+<ul class="list-unstyled">
+  <li>
+    <a href="mailto:flaviacrolla@hotmail.es"><img width="30" height="30" src="img/correo-electronico.svg" alt="Correo"/>Correo</a><br>
+            
+  </li>
+  <li>
+  <a href="https://www.google.com.ar/maps/place/Facultad+de+Ciencia+y+Tecnolog%C3%ADa+(UADER)/@-31.8335955,-60.5221568,17.81z/data=!4m5!3m4!1s0x95b44b973b15bedd:0xa59a2314fe8063f2!8m2!3d-31.8341498!4d-60.5219188"><img width="30" height="30" src="img/localizar.svg" alt="Ubicacion"/>FCyT</a></a>
+  </li>
+</ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- cajita del medio -->
+  </div>
+
+
+
+<div class="conteiner-cr">
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 1995-<?php echo date("Y")?> Copyright:
+    <a href="https://instagram.com/flaapu">flaapu srl</a>
+  </div>
+  <!-- Copyright -->
+</div>
+
+
+
+
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -53,3 +131,5 @@
 <br>
 <br>
 </html>
+<!-- Footer -->
+</footer>
